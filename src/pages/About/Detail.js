888c3732426eec65873be8f8f90b3369a247b0ec
nando8 <fitranurakbar378@gmail.com>
2022-10-26 22:48:42 +0700

    follow me image

--- src/pages/About/Detail.js
+++ src/pages/About/Detail.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function AboutDetail() {
   return (
-    <>
+    <Link to={"https://www.instagram.com/fit.tra_/"}>
       <h2>Fitra Nurakbar</h2>
       <div>
         <img
@@ -12,6 +13,6 @@ export default function AboutDetail() {
         />
         <p>Software Enginer</p>
       </div>
-    </>
+    </Link>
   );
 }
